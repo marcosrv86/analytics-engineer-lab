fa449e5e99395877c350db9508b60c3c981d2f08
marcos.r.vargas.080@gmail.com <marcos.r.vargas.080@gmail.com>
2026-07-18 23:35:35 +0000

    cambio a necio de agro

--- models/example/stg_rendimiento_agro.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT 
-    id_lote,
-    fecha_cosecha,
-    toneladas_netas AS tn_netas,
-    hectareas_sembradas AS ha_sembradas
-FROM 
-    `analytics-lab-sandbox.raw_data.cosecha_2026`
