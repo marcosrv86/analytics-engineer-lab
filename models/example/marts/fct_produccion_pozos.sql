b2c0d0a9e611f254ab5a287796ab631d27a58071
Marcos Vargas <marcos.r.vargas.080@gmail.com>
2026-07-25 02:32:05 +0000

    CTE

--- models/example/marts/fct_produccion_pozos.sql
+++ models/example/marts/fct_produccion_pozos.sql
@@ -1,15 +1,27 @@
 {{ config(materialized='table') }}
 
+WITH extraccion AS (
+    SELECT * FROM {{ ref('stg_extraccion_pozos') }}
+),
+
+geografia AS (
+    SELECT * FROM {{ ref('mapa_pozos') }}
+)
+
 SELECT 
-    id_pozo,
-    MIN(fecha_medicion) AS primera_extraccion,
-    MAX(fecha_medicion) AS ultima_extraccion,
-    SUM(barriles_extraidos) AS total_barriles,
-    -- Aquí inyectamos nuestra macro
-    {{ convertir_barriles_a_m3('SUM(barriles_extraidos)') }} AS total_metros_cubicos,
-    SUM(horas_operacion) AS total_horas,
-    (SUM(barriles_extraidos) / NULLIF(SUM(horas_operacion), 0)) AS rendimiento_promedio_historico
+    e.id_pozo,
+    g.cuenca,
+    g.provincia,
+    MIN(e.fecha_medicion) AS primera_extraccion,
+    MAX(e.fecha_medicion) AS ultima_extraccion,
+    SUM(e.barriles_extraidos) AS total_barriles,
+    {{ convertir_barriles_a_m3('SUM(e.barriles_extraidos)') }} AS total_metros_cubicos,
+    SUM(e.horas_operacion) AS total_horas
 FROM 
-    {{ ref('stg_extraccion_pozos') }}
+    extraccion e
+LEFT JOIN 
+    geografia g ON e.id_pozo = g.id_pozo
 GROUP BY 
-    id_pozo
+    e.id_pozo,
+    g.cuenca,
+    g.provincia
