fd9659da2d6be04aafbd8f9254100cc5be196d7c
Marcos Vargas <marcos.r.vargas.080@gmail.com>
2026-07-25 01:45:42 +0000

    DRY

--- models/example/marts/fct_produccion_pozos.sql
+++ models/example/marts/fct_produccion_pozos.sql
@@ -5,8 +5,9 @@ SELECT
     MIN(fecha_medicion) AS primera_extraccion,
     MAX(fecha_medicion) AS ultima_extraccion,
     SUM(barriles_extraidos) AS total_barriles,
+    -- Aquí inyectamos nuestra macro
+    {{ convertir_barriles_a_m3('SUM(barriles_extraidos)') }} AS total_metros_cubicos,
     SUM(horas_operacion) AS total_horas,
-    -- Calculamos la eficiencia histórica del pozo
     (SUM(barriles_extraidos) / NULLIF(SUM(horas_operacion), 0)) AS rendimiento_promedio_historico
 FROM 
     {{ ref('stg_extraccion_pozos') }}
