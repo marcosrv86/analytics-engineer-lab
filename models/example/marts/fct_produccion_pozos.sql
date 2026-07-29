d489046e0b87fb8bfa679150b275d4c84de418bb
Marcos Vargas <marcos.r.vargas.080@gmail.com>
2026-07-29 02:27:35 +0000

    metrica

--- models/example/marts/fct_produccion_pozos.sql
+++ models/example/marts/fct_produccion_pozos.sql
@@ -16,7 +16,7 @@ SELECT
     MAX(e.fecha_medicion) AS ultima_extraccion,
     SUM(e.barriles_extraidos) AS total_barriles,
     {{ convertir_barriles_a_m3('SUM(e.barriles_extraidos)') }} AS total_metros_cubicos,
-   SUM(e.horas_operacion) AS hs_operacion_total
+   SUM(e.horas_operacion) AS horas_trabajada
 FROM 
     extraccion e
 LEFT JOIN 
