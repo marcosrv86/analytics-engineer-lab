e4904fe83d436d86f367b09e72f899a4c1b70da2
Marcos Vargas <marcos.r.vargas.080@gmail.com>
2026-08-10 00:09:18 +0000

    cambio en jinja y nuevo paquete de dbt

--- models/example/marts/mart_produccion_pivot.sql
+++ models/example/marts/mart_produccion_pivot.sql
@@ -1,22 +1,13 @@
--- 1. Definimos una variable Jinja que contiene una lista (array) de nuestras provincias
-{% set provincias = ['Santa Cruz', 'Salta', 'Chubut', 'Mendoza', 'Neuquén'] %}
+SELECT
+  id_pozo,
+  
+  -- La macro pivot escanea la tabla fct_produccion_pozos, encuentra todas las 
+  -- provincias únicas y construye las columnas dinámicamente sumando los barriles.
+  {{ dbt_utils.pivot(
+      'provincia',
+      dbt_utils.get_column_values(ref('fct_produccion_pozos'), 'provincia'),
+      then_value='barriles'
+  ) }}
 
-WITH pivot_datos AS (
-    SELECT
-        id_pozo,
-        
-        -- 2. Iniciamos el bucle. Por cada provincia en la lista, escribe lo siguiente:
-        {% for provincia in provincias %}
-        
-                SUM(CASE WHEN provincia = '{{ provincia }}' THEN total_barriles ELSE 0 END) AS barriles_{{ provincia | replace(' ', '_') | replace('é', 'e') | lower }}
-        
-        -- 3. Lógica para las comas: Si NO es la última iteración del bucle, pon una coma.
-        {% if not loop.last %} , {% endif %}
-        
-        {% endfor %}
-        
-    FROM {{ ref('fct_produccion_pozos') }}
-    GROUP BY 1
-)
-
-SELECT * FROM pivot_datos
+FROM {{ ref('fct_produccion_pozos') }}
+GROUP BY id_pozo
